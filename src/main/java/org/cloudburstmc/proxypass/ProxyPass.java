b088cf2d07b039c34ee04d39d1d5ecc2156e2267
Kas-tle <26531652+Kas-tle@users.noreply.github.com>
2024-08-14 10:46:55 -0700

    1.21.20 (#5)
    Signed-off-by: GitHub <noreply@github.com>

--- src/main/java/org/cloudburstmc/proxypass/ProxyPass.java
+++ src/main/java/org/cloudburstmc/proxypass/ProxyPass.java
@@ -29,7 +29,7 @@ import org.cloudburstmc.protocol.bedrock.BedrockPeer;
 import org.cloudburstmc.protocol.bedrock.BedrockPong;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
-import org.cloudburstmc.protocol.bedrock.codec.v686.Bedrock_v686;
+import org.cloudburstmc.protocol.bedrock.codec.v712.Bedrock_v712;
 import org.cloudburstmc.protocol.bedrock.data.EncodingSettings;
 import org.cloudburstmc.protocol.bedrock.data.definitions.BlockDefinition;
 import org.cloudburstmc.protocol.bedrock.netty.initializer.BedrockChannelInitializer;
@@ -64,8 +64,8 @@ public class ProxyPass {
     public static final ObjectMapper JSON_MAPPER;
     public static final YAMLMapper YAML_MAPPER = (YAMLMapper) new YAMLMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     public static final String MINECRAFT_VERSION;
-    public static final BedrockCodecHelper HELPER = Bedrock_v686.CODEC.createHelper();
-    public static final BedrockCodec CODEC = Bedrock_v686.CODEC
+    public static final BedrockCodecHelper HELPER = Bedrock_v712.CODEC.createHelper();
+    public static final BedrockCodec CODEC = Bedrock_v712.CODEC
         .toBuilder().helper(() -> HELPER).build();
         
     public static final int PROTOCOL_VERSION = CODEC.getProtocolVersion();
